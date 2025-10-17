c97e5edc7530e18261503056b6f07bd0d38a3d30
EvanGray <evanmgray94@gmail.com>
2025-10-17 09:40:53 -0700

    Create React Project

---
+++ task-manager-react/src/assets/components/MyFirstComponent.jsx
@@ -0,0 +1,10 @@
+function MyFirstComponent() {
+  return (
+    <div>
+      <h1>My First Component</h1>
+      <p>This is my first React component!</p>
+    </div>
+  );
+}
+
+export default MyFirstComponent;
